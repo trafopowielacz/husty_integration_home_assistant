69ec96d1d3116dcaf6aadda2cade53511ec1bf3c
trafopowielacz <37472572+trafopowielacz@users.noreply.github.com>
2026-07-15 22:02:21 +0200

    Update binary_sensor.py

--- custom_components/husty/binary_sensor.py
+++ custom_components/husty/binary_sensor.py
@@ -1,8 +1,36 @@
-from homeassistant.components.binary_sensor import (
-    BinarySensorEntity
-)
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import DOMAIN
+
+
+BINARY_SENSORS = {
+
+    "online": {
+        "name": "Online",
+        "path": [
+            "metadata",
+            "connectionState"
+        ],
+    },
+
+    "regeneration": {
+        "name": "Regeneracja aktywna",
+        "path": [
+            "core",
+            "regenerationInProgress"
+        ],
+    },
+
+    "shutoff_valve": {
+        "name": "Zawór odcinający",
+        "path": [
+            "core",
+            "leakageShutoffValveState"
+        ],
+    },
+
+}
 
 
 
@@ -16,36 +44,100 @@ async def async_setup_entry(
 
     async_add_entities(
         [
-            HustyOnline(coordinator)
+            HustyBinarySensor(
+                coordinator,
+                key,
+                data
+            )
+
+            for key, data in BINARY_SENSORS.items()
         ]
     )
 
 
 
-class HustyOnline(
-    BinarySensorEntity
-):
-
-    _attr_name = "Husty Online"
-
+class HustyBinarySensor(BinarySensorEntity):
 
     def __init__(
         self,
-        coordinator
+        coordinator,
+        key,
+        data
     ):
 
         self.coordinator = coordinator
+
+        self.path = data["path"]
+
+        self._attr_unique_id = (
+            f"husty_{key}"
+        )
+
+        self._attr_name = (
+            f"Husty {data['name']}"
+        )
+
+        self._attr_icon = (
+            "mdi:valve"
+            if key == "shutoff_valve"
+            else "mdi:check-network"
+        )
+
+
+
+    @property
+    def device_info(self):
+
+        data = (
+            self.coordinator.data[1]
+            ["result"]
+            ["data"]
+            ["json"]
+        )
+
+        return DeviceInfo(
+            identifiers={
+                (
+                    DOMAIN,
+                    data["deviceId"]
+                )
+            },
+
+            name="Husty SaoCal 250 LE",
+
+            manufacturer="Husty",
+
+            model=data["core"]["model"],
+
+            sw_version=data["core"]["version"],
+        )
+
 
 
     @property
     def is_on(self):
 
-        return (
+        data = (
             self.coordinator.data[1]
             ["result"]
             ["data"]
             ["json"]
-            ["status"]
-            ==
-            "online"
         )
+
+
+        for item in self.path:
+
+            data = data[item]
+
+
+        if self.path[-1] == "connectionState":
+
+            return data == "Connected"
+
+
+        if self.path[-1] == "leakageShutoffValveState":
+
+            return data == 0
+
+
+        return bool(data)
